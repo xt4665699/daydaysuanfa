742eaac9ec5f743a89c5ecba883f5dc469521530
xietao <173160769@qq.com>
2022-07-04 23:21:00 +0800

    aaa

--- src/two_to_one/quick_sort/QuickSort.java
+++ src/two_to_one/quick_sort/QuickSort.java
@@ -31,6 +31,35 @@ public class QuickSort {
         swap(arr,l,j);
         return j;
     }
+    public static <E extends Comparable<E>> void  sort2ways(E[] arr){
+        sort2ways(arr,0,arr.length - 1);
+    }
+    private static <E extends Comparable<E>> void  sort2ways(E[] arr,int l, int r){
+        if (l >= r) return;
+        int p = partition2(arr,l,r);
+        sort2ways(arr,l, p -1);
+        sort2ways(arr,p, r);
+    }
+    private static <E extends Comparable<E>> int partition2(E[] arr, int l, int r){
+
+        int p = l + (new Random().nextInt( r - l + 1));
+        swap(arr,l,p);
+
+        //arr[l +1 .... i -1]<= v; arr[j+1...r] >=v
+        int i  = l + 1, j = r;
+        while (true){
+            while (i <= j && arr[i].compareTo(arr[l]) < 0)
+                i++;
+            while (j >= i && arr[j].compareTo(arr[l]) > 0)
+                j--;
+            if (i >= j)break;
+            swap(arr,i,j);
+            i++;
+            j++;
+        }
+        swap(arr,i,j);
+        return j;
+    }
 
     private static<E> void swap(E[] arr, int i, int j) {
         E t  = arr[i];
