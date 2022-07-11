f503b1a86c53a02a2aa6437c5c2489a903ef8c17
xietao <173160769@qq.com>
2022-07-11 11:53:45 +0800

    去去去

---
+++ src/tow_to_four/BST.java
@@ -0,0 +1,48 @@
+package tow_to_four;
+
+public class BST<E extends Comparable<E>> {
+    private class Node{
+        public E e;
+        public Node left,right;
+
+        public Node(E e) {
+            this.e = e;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+    public int size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void add(E e){
+        root = add(root,e);
+    }
+
+    private Node add(Node node, E e) {
+
+        if (node ==  null){
+            size++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(node.e) < 0){
+            node.left = add(node.left,e);
+        }else {
+            node.right = add(node.right,e);
+        }
+
+        return node;
+    }
+}
