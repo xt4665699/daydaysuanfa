00890bcb587ac5c212926c4eeb8d9a1935191cd2
xietao <173160769@qq.com>
2022-07-20 13:19:20 +0800

    去去去

--- src/two_to_four/BST.java
+++ src/two_to_four/BST.java
@@ -186,5 +186,42 @@ public class BST<E extends Comparable<E>> {
         return node;
     }
 
+    public void remove(E e){
+        root = remove(root,e);
+    }
+    private Node remove(Node node,E e){
+        if (node == null){
+            return null;
+        }
+        if (e.compareTo(node.e) < 0 ){
+            node.left = remove(node.left,e);
+            return node;
+        } else if (e.compareTo(node.e) > 0 ){
+            node.right = remove(node.right,e);
+            return node;
+        }else {
+            if (node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+
+            if (node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            return successor;
+        }
+
+
+    }
 
 }
